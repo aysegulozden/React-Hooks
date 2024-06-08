df0f06db2b057a207681887763ed9b9ca7105f4a
Aysegul Ozden <ozdenaysegul728@gmail.com>
2024-06-09 00:54:50 +0300

    useReducer

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './App.css'
+import { useReducer } from 'react'
+
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialValue;
+    default:
+      return state;
+
+  }
+
+}
+
+function App() {
+
+
+  const [count, dispatch] = useReducer(reducer, initialValue);
+
+
+  return (
+    <div className='app'>
+      <div>Sayı={count}</div>
+      <button onClick={() => dispatch('increment')}>Arttır</button>
+      <button onClick={() => dispatch('decrement')}>Azalt</button>
+      <button onClick={() => dispatch('reset')}>Reset</button>
+
+    </div>
+  )
+}
+
+export default App
